11b3812b99f1f1e4a2909fbbf5a6b5f7a8ed7c60
muzahid37 <muzahidul372709@gmail.com>
2022-07-02 00:24:45 +0600

    patch the api

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://daily_task_admin:55L7VQez!eCr4Lq@cluster0.0glbt.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0glbt.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
